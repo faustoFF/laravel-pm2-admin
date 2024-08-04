c62496702f3a5bedb31e6caab2de1c777b8499d1
faustoFF <kulakov89@gmail.com>
2024-08-04 17:01:31 +0300

    moved middleware to config

--- config/pm2-admin.php
+++ config/pm2-admin.php
@@ -1,5 +1,12 @@
 <?php
 
+use Faustoff\LaravelPm2Admin\Http\Middleware\Authorize;
+
 return [
-    'api_server_address' => env('PM2_ADMIN_API_SERVER_ADDRESS', 'pm2:8000')
+    'api_server_address' => env('PM2_ADMIN_API_SERVER_ADDRESS', 'pm2:8000'),
+
+    'middleware' => [
+        'web',
+        Authorize::class,
+    ]
 ];
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Faustoff\LaravelPm2Admin\Http\Controllers\ApiController;
 use Faustoff\LaravelPm2Admin\Http\Controllers\WebController;
-use Faustoff\LaravelPm2Admin\Http\Middleware\Authorize;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('pm2')
-    ->middleware([
-        'web',
-        Authorize::class,
-    ])
+    ->middleware(config('pm2-admin.middleware'))
     ->group(function () {
         Route::get('api', ApiController::class);
         Route::get('', WebController::class);
